5b630b429445983d8f93a8d7c13fe4373cfa9ae2
Methuselah-Nwodobeh <methuselah.nwodobeh@amalitech.com>
2024-07-29 11:26:23 +0000

    initial commit

---
+++ src/main/java/com/tlc/ct_search/CtSearchApplication.java
@@ -0,0 +1,13 @@
+package com.tlc.ct_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tlc/ct_search/CtSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlc.ct_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
